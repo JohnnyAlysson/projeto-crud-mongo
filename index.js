0e8a921f294c800ab4a04ed3343266343218dff2
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2024-10-14 23:40:07 -0300

    0.1.0 feat : add all routes, organize project working

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const { restart } = require("nodemon");
 const fs = require("fs").promises;
 const app = express();
 
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 async function getPasswordFromJson() {
   try {
@@ -18,17 +20,10 @@ async function getPasswordFromJson() {
   }
 }
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
+app.use("/api/products", productRoute);
 
-app.post('/api/products', async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(201).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+app.get("/", (req, res) => {
+  res.send("HELLO IT'S WORKING");
 });
 
 async function startServer() {
